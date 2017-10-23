bd42d542d0ea69053d1cfc7b520cbd4b49f6288a
Mads Nielsen <31951027+TheCaptain420@users.noreply.github.com>
2017-10-23 11:31:34 +0200

    ayyy lmao:

---
+++ src/TrycatchSUM.java
@@ -0,0 +1,31 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class TrycatchSUM {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        int num1 = 5;
+        int num3 = 5;
+        int acsum = num1 + num3;
+        int sum = 0;
+        while (sum != acsum) {
+            try {
+                System.out.println("What is the sum of " + num1 + " + " + num3);
+                sum = input.nextInt();
+
+            } catch (InputMismatchException ex) {
+                System.out.println("Fejl, du skal indtaste en int.");
+                break;
+            }
+
+            if (sum == acsum) {
+                System.out.println("True");
+            } else {
+                System.out.println("false");
+            }
+        }
+    }
+
+}
